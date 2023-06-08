b06b90667ff05f229e426992b288dcc679eded14
shin yong cheol <toyy1004kr@naver.com>
2023-06-08 10:58:27 +0900

    20230608-1

---
+++ SycModule/src/main/java/com/syc/sycmodule/EmptyRecyclerView.kt
@@ -0,0 +1,55 @@
+package com.syc.sycmodule
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class RecyclerViewEmptySupport : RecyclerView {
+
+    private var emptyView: View? = null
+    private val emptyObserver: AdapterDataObserver = object : AdapterDataObserver() {
+        override fun onChanged() {
+
+            val adapter = adapter
+
+            if (adapter != null && emptyView != null) {
+                if (adapter.itemCount == 0) {
+                    emptyView!!.visibility = View.VISIBLE
+                    this@RecyclerViewEmptySupport.visibility = View.GONE
+                } else {
+                    emptyView!!.visibility = View.GONE
+                    this@RecyclerViewEmptySupport.visibility = View.VISIBLE
+                }
+            }
+
+        }
+    }
+
+    override fun setAdapter(adapter: Adapter<*>?) {
+        super.setAdapter(adapter)
+        adapter?.registerAdapterDataObserver(emptyObserver)
+        emptyObserver.onChanged()
+    }
+
+    fun setEmptyView(emptyView: View?) {
+        this.emptyView = emptyView
+    }
+
+    lateinit var mTextView: TextView
+
+    private fun createEmptyView() {
+
+    }
+
+    constructor(context: Context) : super(context) {}
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int
+    ) : super(context, attrs, defStyleAttr) {
+    }
+}
--- SycModule/src/main/java/com/syc/sycmodule/SycUtil.kt
+++ SycModule/src/main/java/com/syc/sycmodule/SycUtil.kt
@@ -5,6 +5,7 @@ import android.util.Log
 class SycUtil {
 
     companion object {
+
         fun log(log: String) {
             Log.d("sycUtil","##################START#################")
             Log.d("sycUtil",log)
